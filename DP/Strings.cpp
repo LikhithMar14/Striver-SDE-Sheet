348e435565938171f6a38cbf1ed048b9bda47136
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-16 10:00:45 +0530

    update DP

--- DP/Strings.cpp
+++ DP/Strings.cpp
@@ -74,3 +74,53 @@ class Solution {
         return maxi;
     }
 };
+
+
+//Minium Insertions to make a string palindrome
+
+class Solution {
+    public:
+        int minInsertions(string s) {
+            //no of insertions will be equal to totallength-lengthofmaxsubsequecne
+    
+            int n = s.size();
+            string r = s;
+            reverse(r.begin(),r.end());
+            vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    
+            for(int i = n-1; i >= 0; i--){
+                for(int j=n-1; j >= 0; j--){
+                    if(s[i] == r[j])dp[i][j] = 1+dp[i+1][j+1];
+                    else{
+                        dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                    }
+                }
+            }
+            return s.size()-dp[0][0];
+        }
+    };
+
+//Delete operation for two strings
+
+class Solution {
+    public:
+        int minDistance(string word1, string word2) {
+            //find the longest subsequenece(because index doesnet bmatter for delteion) it legnth
+            //formual s1+s2-2*lengthoflongsubstring
+    
+            int n = word1.size();
+            int m = word2.size();
+            vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    
+            for(int i=n-1; i >= 0; i--){
+                for(int j=m-1; j >= 0; j--){
+                    if(word1[i] == word2[j]){
+                        dp[i][j] = 1+dp[i+1][j+1];
+                    }else{
+                        dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                    }
+                }
+            }
+            return n+m-2*dp[0][0];
+        }
+    };
