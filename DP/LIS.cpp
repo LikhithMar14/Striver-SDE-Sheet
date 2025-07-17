fe7946a4094165be91db35ddee04b03197dba337
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-18 00:58:58 +0530

    update DP

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -163,3 +163,35 @@ public:
         return maxi;
     }
 };
+
+// Number of Longest Increasing Subsequence
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        vector<int> count(n, 1);
+        int maxi = 1;
+
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i+1; j < n; j++) {
+                if(nums[i] < nums[j]) {
+                    if(dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    } else if(dp[j] + 1 == dp[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+
+        int total = 0;
+        for(int i = 0; i < n; i++) {
+            if(dp[i] == maxi) total += count[i];
+        }
+        return total;
+    }
+};
