5d620689a65e57e0cd7252c03084adb1f1a7f6ce
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-18 20:24:26 +0530

    update DP

--- DP/MatrixChainMultiplication.cpp
+++ DP/MatrixChainMultiplication.cpp
@@ -25,3 +25,34 @@ public:
         return dp[0][c-1];
     }
 };
+
+//312. Burst Balloons
+
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        vector<int> arr;
+        arr.push_back(1);
+        arr.insert(arr.end(), nums.begin(), nums.end());
+        arr.push_back(1);
+
+        int c = arr.size();
+        vector<vector<int>> dp(c, vector<int>(c, 0));
+
+        for (int length = 2; length < c; length++) {
+            for (int i = 0; i + length < c; i++) {
+                int j = i + length;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = max(dp[i][j],
+                    //very important here
+                        arr[i] * arr[k] * arr[j] + dp[i][k] + dp[k][j]
+                    );
+                }
+            }
+        }
+        return dp[0][c-1];
+    }
+};
+
+
