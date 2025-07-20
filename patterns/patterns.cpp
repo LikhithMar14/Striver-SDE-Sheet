a239d5493e05dfb69b792650478c93bde660ddb2
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-21 02:24:54 +0530

    Commit

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+#include <string>
 //Recursion and Backtracking
 //Binary Exponentiation
 
@@ -6,7 +8,7 @@ class Solution {
         long long mod = 1e9 + 7;
         //important
         long long mulMod(long long a, long long b) {
-            return (a % mod * b % mod) % mod;
+            return (1LL*a % mod * b % mod) % mod;
         }
     
         long long helper(long long n, long long k) {
@@ -28,4 +30,66 @@ class Solution {
         }
     };
     
+//BackTrack use address to persist , so we can do physical backtracking and get fine grained control
+//Handles not take cases automcatically
+
+
+class Solution {
+    public:
+        void solve(int idx, vector<string>&res, string temp,int n){
+            if(temp.size() == n){
+                res.push_back(temp);
+                return;
+            }
+            //pick 0
+            temp.push_back('0');
+            solve(idx+1,res,temp,n);
+            temp.pop_back();
+            if((idx > 0 && temp[idx-1] == '0') || idx == 0){
+                temp.push_back('1');
+                solve(idx+1,res,temp,n);
+                temp.pop_back();
+            }
+           
     
+        }
+        vector<string> generateBinaryStrings(int n) {
+            // Your code goes here
+            vector<string>res;
+            string temp = "";
+            solve(0,res,temp,n);
+            return res;
+        }
+    };
+    
+
+//For Loop Pattern for recursion backtracking
+
+class Solution {
+    public:
+        void solve(int idx, int n,vector<int>&c,vector<int>&temp,vector<vector<int>>&res,int target,int sum){
+           
+            if(target == sum){
+                res.push_back(temp);
+                return;
+            }
+            
+            for(int i = idx; i < n; i++){
+                if(i != idx && c[i] == c[i-1]){
+                    continue;
+                }
+                if(sum+c[i] > target)break;//it is sorted
+                temp.push_back(c[i]);
+                solve(i+1,n,c,temp,res,target,sum+c[i]);
+                temp.pop_back();
+            }
+        }
+        vector<vector<int>> combinationSum2(vector<int>& c, int target) {
+            sort(c.begin(),c.end());
+            //order wont matter and also easy to differentiate
+            vector<int>temp;
+            vector<vector<int>>res;
+            solve(0,c.size(),c,temp,res,target,0);
+            return res;
+        }
+};
