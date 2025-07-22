f63182e265d6b2a488a4c6721ec2bbe93187f105
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-22 11:34:30 +0530

    update

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -93,3 +93,34 @@ class Solution {
             return res;
         }
 };
+
+//Variable size sliding window
+class Solution {
+    public:
+        int longestOnes(vector<int>& nums, int k) {
+            int n = nums.size();
+    
+            int i = 0;
+            int j = 0;
+            map<int,int>mp;
+            int ans = 0;
+    
+    
+            while(j < n){
+                //=> Important point to note these growth and decreasing stages can be flipped according to needs
+                
+                //Growth Stage
+                mp[nums[j]]++;
+                //decreasing stage on condition
+                while(i <= j && mp[0] > k){
+                    mp[nums[i]]--;
+                    i++;
+                }
+                //answer calculation
+                ans = max(ans,j-i+1);
+                j++;
+            }
+            return ans;
+            
+        }
+    };
