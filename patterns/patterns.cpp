855e955fd2fda816868fc6fd16f8a45b102f7542
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-23 20:49:26 +0530

    Commit

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -93,6 +93,42 @@ class Solution {
             return res;
         }
 };
+//very good use case of elimination using 2ptr
+
+class Solution {
+    public:
+        int celebrity(vector<vector<int>>& mat) {
+            int n = mat.size();
+            int i = 0;
+            int j = n - 1;
+    
+    
+            while (i < j) {
+                if (mat[i][j] == 1) {
+    
+                    i++;
+                } else {
+    
+                    j--;
+                }
+            }
+    
+            int candidate = i;
+    
+    
+            for (int k = 0; k < n; k++) {
+                if (k == candidate) continue;
+    
+    
+                if (mat[candidate][k] == 1 || mat[k][candidate] == 0) {
+                    return -1;
+                }
+            }
+    
+            return candidate;
+        }
+    };
+    
 
 //Variable size sliding window
 class Solution {
@@ -108,7 +144,7 @@ class Solution {
     
             while(j < n){
                 //=> Important point to note these growth and decreasing stages can be flipped according to needs
-                
+
                 //Growth Stage
                 mp[nums[j]]++;
                 //decreasing stage on condition
@@ -124,3 +160,112 @@ class Solution {
             
         }
     };
+
+class Solution {
+    public:
+        int solve(int n, int goal,vector<int>&nums){
+            int i = 0;
+            int j = 0;
+            int ans = 0;
+            int cnt = 0;
+    
+            while(j < n){
+                cnt += nums[j];
+                while(i <= j && cnt > goal){
+                    cnt -= nums[i];
+                    i++;
+                }
+                //all subarrays who are less than goals
+                ans += (j-i+1);
+    
+                j++;
+            }
+            return ans;
+            
+    
+        }
+        int numSubarraysWithSum(vector<int>& nums, int goal) {
+            //very important question
+            //here they given one thing should be equal so we should do increasing and decreaseing at a same time not possibe
+    
+            //cleverly do == k => k-k-1
+    
+            return (solve(nums.size(),goal,nums)-solve(nums.size(),goal-1,nums));
+        }
+    };
+//fixed size template
+class Solution {
+    public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+        int maxi = INT_MIN;
+
+
+        for(int i=0; i < n; i++){
+            sum += nums[i];
+            if(i >= k-1){
+                //when at last valid element
+                maxi = max(maxi,sum);
+                sum -= nums[i-k+1];
+            }
+        }
+        return (double)maxi/k;
+    }
+};
+
+//Circular Array Logic + Monotonic Stack
+
+
+class Solution {
+    public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        //Circular arry logic
+
+        // 1 2 1 1 2 1
+        //feed the elements in the stack from right to left so that we can access them later
+
+        vector<int>res(nums.size(),-1);
+        stack<int>st;
+        int n = nums.size();
+
+        for(int i=2*n-1; i >= 0; i--){
+            while(st.size() && st.top() <= nums[i%n])st.pop();
+            if(!st.empty() && (res[i%n] == -1))res[i%n] = st.top();
+            st.push(nums[i%n]);
+        }
+        return res;
+
+    }
+};
+//Very Very Imporattnt gives conceptual clarity on monotonic stack
+
+class Solution {
+    public:
+        int trap(vector<int>& height) {
+            int n = height.size();
+            int res = 0;
+            stack<int> st; 
+    
+            for (int i = 0; i < n; i++) {
+    
+                while (!st.empty() && height[i] > height[st.top()]) {
+                    int bottom = st.top();
+                    st.pop();
+    
+                    if (st.empty()) break; 
+    
+                    int left = st.top();
+                    int width = i - left - 1;
+                    int boundedHeight = min(height[i], height[left]) - height[bottom];
+    
+                    res += width * boundedHeight;
+                }
+    
+                st.push(i);
+            }
+    
+            return res;
+        }
+    };
+    
