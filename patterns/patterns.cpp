0304c80c7df227f3f1a7e4498f9a51caff9872b3
2005LIKHITH <likhithcvsrl@gmail.com>
2025-07-27 15:50:51 +0530

    Update

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -298,3 +298,38 @@ class Solution {
         
     }
     };
+//Bit Manipulation
+
+
+class Solution {
+    public:
+        int divide(int dividend, int divisor) {
+            //because INT_MIN is 1 greater than the INT_MAX in absolute termsd
+      
+            if (dividend == INT_MIN && divisor == -1)
+                return INT_MAX;
+            if (dividend == INT_MIN && divisor == 1)
+                return INT_MIN;
+            if (dividend == divisor)
+                return 1;
+    
+            bool isPos = (dividend >= 0) == (divisor >= 0);
+    
+          
+            long n = abs((long)dividend);
+            long d = abs((long)divisor);
+            int ans = 0;
+    
+            while (n >= d) {
+                int i = 0;
+                while (n >= (d << (i + 1))) {
+                    i++;
+                }
+                ans += (1 << i);       
+                n -= (d << i);        
+            }
+    
+            return isPos ? ans : -ans;
+        }
+    };
+    
